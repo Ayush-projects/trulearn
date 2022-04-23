356b071443ccc4e9a6a020f5fa1812601475a6f3
Ayush <kayush479@gmail.com>
2022-04-23 17:51:22 +0530

    AYush

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ window.mobileAndTabletCheck = function() {
 if(mobileAndTabletCheck())
 {
     document.getElementById("lol").style.display = "none";
-    document.getElementById("bottom").style.top = "95em"
+    document.getElementById("bottom").style.top = "85em"
 }
 
 (function($) {
